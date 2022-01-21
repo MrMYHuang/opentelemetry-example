b185950f7e61b9ad12aa7a84a59a5ff9933d8bac
Roger Huang <roger@beautimode.com>
2022-01-21 11:55:50 +0800

    * Support tracing.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 import express from 'express';
+
+import * as opentelemetry from '@opentelemetry/api';
+
+import { BasicTracerProvider, SimpleSpanProcessor, ConsoleSpanExporter } from '@opentelemetry/sdk-trace-base';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+
 import { MeterProvider, ConsoleMetricExporter } from '@opentelemetry/sdk-metrics-base';
 import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
 
 const port = 8089;
+
+const tracerProvider = new BasicTracerProvider();
+tracerProvider.addSpanProcessor(new SimpleSpanProcessor(
+    new OTLPTraceExporter({
+        url: 'http://tempo-otlp/v1/traces'
+    })
+));
+tracerProvider.register();
+const tracer = opentelemetry.trace.getTracer('myTracer');
+
 
 const meter = new MeterProvider({
     exporter: new PrometheusExporter({
@@ -15,6 +31,15 @@ const boundInstrumenets = new Map();
 
 const app = express();
 
+function doWork(parentSpan: opentelemetry.Span) {
+    const ctx = opentelemetry.trace.setSpan(opentelemetry.context.active(), parentSpan);
+    const span = tracer.startSpan('doWork', undefined, ctx);
+    for (let i = 0; i < 1e9; i++) {
+        Math.sqrt(i);
+    }
+    span.end();
+}
+
 app.use((req, res, next) => {
     if (!boundInstrumenets.has(req.path)) {
         const requestCount = meter.createCounter(`requests${req.path.replace('/', '_')}`, {
@@ -24,6 +49,12 @@ app.use((req, res, next) => {
     }
 
     boundInstrumenets.get(req.path).add(1);
+
+    const span = tracer.startSpan('main');
+    doWork(span);
+    console.log(span.spanContext().traceId);
+    span.end();
+
     next();
 });
 
