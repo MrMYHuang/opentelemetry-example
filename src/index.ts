9d320303891c51b0f8bbc55ef6de4b0b38dddabe
Meng-Yuan Huang <myh@live.com>
2022-09-08 10:38:28 +0800

    * Fix bugs.

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,5 @@
 import express from 'express';
-
-import * as opentelemetry from '@opentelemetry/sdk-node';
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
-
 const port = 8080;
-const sdk = new opentelemetry.NodeSDK({
-    traceExporter: new opentelemetry.tracing.ConsoleSpanExporter(),
-    instrumentations: [getNodeAutoInstrumentations()]
-});
-
-sdk.start();
-
-const sdk2 = new opentelemetry.NodeSDK({
-    traceExporter: new OTLPTraceExporter({
-        url: 'http://tempo.loki:4318/v1/traces'
-    }),
-    instrumentations: [getNodeAutoInstrumentations()]
-});
-
-sdk2.start();
 
 const app = express();
 
---
+++ src/trace.ts
@@ -0,0 +1,25 @@
+import * as opentelemetry from '@opentelemetry/sdk-node';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+
+/** /
+const sdk = new opentelemetry.NodeSDK({
+    traceExporter: new opentelemetry.tracing.ConsoleSpanExporter(),
+    instrumentations: [getNodeAutoInstrumentations()]
+});
+/**/
+
+/**/
+const sdk = new opentelemetry.NodeSDK({
+    traceExporter: new OTLPTraceExporter({
+        url: 'http://tempo.loki:4318/v1/traces'
+    }),
+    instrumentations: [getNodeAutoInstrumentations()]
+});
+/**/
+
+sdk.start();
+
